8f6170ebf58a59f767fe2b76a597b38c3fe71927
jrazec <johnrazeagno@gmail.com>
2025-07-05 01:11:38 +0800

    Here's tmx file in the commit below

---
+++ assets/tiles/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="map" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="../images/map-tileset.png" width="512" height="256"/>
+</tileset>
